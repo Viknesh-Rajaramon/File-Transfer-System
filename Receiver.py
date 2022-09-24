d56e91aff204cc8401d64b7c5bb0342cce6ad551
Viknesh Rajaramon <viknesh0209@gmail.com>
2022-09-24 18:48:17 +0530

    update codes

---
+++ Receiver.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'recv.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+import socket
+import tqdm
+import os
+
+
+SEPARATOR = "<SEPARATOR>"
+BUFFER_SIZE = 1024
+
+
+class Ui_RecvWindow(object):     
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(722, 363)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(120, 10, 611, 131))
+        font = QtGui.QFont()
+        font.setFamily("Noto Serif CJK SC")
+        font.setPointSize(36)
+        font.setUnderline(True)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(40, 150, 181, 41))
+        self.label_2.setObjectName("label_2")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(170, 160, 501, 23))
+        self.lineEdit.setObjectName("lineEdit")
+        self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
+        self.progressBar.setGeometry(QtCore.QRect(40, 220, 631, 41))
+        self.progressBar.setProperty("value", 0)
+        self.progressBar.setObjectName("progressBar")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(250, 280, 281, 41))
+        font = QtGui.QFont()
+        font.setFamily("Liberation Serif")
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton.setFont(font)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 722, 20))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.pushButton.clicked.connect(self.server)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Start Receiving"))
+        self.label.setText(_translate("MainWindow", "File Transfer Server"))
+        self.label_2.setText(_translate("MainWindow", "Connection status :"))
+        self.lineEdit.setText(_translate("MainWindow", "No connection yet"))
+
+    def server(self):
+        SERVER_HOST = "0.0.0.0"
+        SERVER_PORT=5001
+        s=socket.socket()
+        s.bind((SERVER_HOST,SERVER_PORT))
+        s.listen(1)
+        value = 0
+        print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+        client_socket, address = s.accept() 
+        print(f"[+] {address} is connected.")
+        self.lineEdit.setText(f"Connected to {address[0]}.")
+
+        received = client_socket.recv(BUFFER_SIZE).decode()
+        filename, filesize = received.split(SEPARATOR)
+        filename = os.path.basename(filename)
+        filesize = int(filesize)
+
+        progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+        with open(filename, "wb") as f:
+            for _ in progress:
+                bytes_read = client_socket.recv(BUFFER_SIZE)
+                value = value + 1024
+                self.progressBar.setProperty("value", value/filesize*100)
+                if not bytes_read:
+                    break
+                f.write(bytes_read)
+                progress.update(len(bytes_read))
+        client_socket.close()
+        s.close()
+        
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_RecvWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
